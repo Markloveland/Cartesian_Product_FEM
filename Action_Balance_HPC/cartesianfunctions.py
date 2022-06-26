30575545020cb3cd386cd09c6996eb95bd77ace3
Mark Loveland <markloveland@utexas.edu>
2022-06-26 13:01:51 -0500

    modularizing code

--- Action_Balance_HPC/cartesianfunctions.py
+++ Action_Balance_HPC/cartesianfunctions.py
@@ -1,5 +1,11 @@
 import numpy as np
 from fenics import *
+import petsc4py
+petsc4py.init()
+from petsc4py import PETSc
+from mpi4py import MPI
+from scipy import sparse as sp
+
 #Some auxillary functions defined first
 def cartesian_product(array1,array2):
     #gives cartesian product of 2 vectors
@@ -110,10 +116,10 @@ def assemble_global_CSR(Arow,Acol,Brow,Bcol,dat):
     nnzB = Brow[1:] - Brow[:-1]
     nA = len(nnzA)
     nB = len(nnzB)
-    print('Num A')
-    print(len(nnzA))
-    print('Num B')
-    print(len(nnzB))
+    #print('Num A')
+    #print(len(nnzA))
+    #print('Num B')
+    #print(len(nnzB))
     Kcol = np.zeros(dat.size)
     Krow = np.zeros(len(nnzA)*len(nnzB)+1)
     Kdat = np.zeros(dat.size)
@@ -138,4 +144,184 @@ def assemble_global_CSR(Arow,Acol,Brow,Bcol,dat):
         j0=j0+n1
     
     return Krow,Kcol,Kdat
+def create_cartesian_mass_matrix(local_rows,global_rows,local_cols,global_cols):
+    #Allocate global mass matrix
+    #need to generate global mass matrix to get global matrix layout and sparsity patterns
+    #first I need to create an mpi matrix of the appropriate size and start storing values
+    M = PETSc.Mat()
+    M.create(comm=MPI.COMM_WORLD)
+    M.setSizes(([local_rows,global_rows],[local_cols,global_cols]),bsize=1)
+    M.setFromOptions()
+    M.setType('aij')
+    M.setUp()
+    #also need global stiffness matrix
+    #same exact structure as M
+    return M
+
+def build_cartesian_mass_matrix(M1_pet,M2_pet,M1_sizes,M1_global_size,M2_sizes,M):
+    #takes in 2 Fenics tensors and creates PETSc matrix entries in
+    #CSR format
+    ###################################################################
+    #Preallocate!
+    #to get nnz of cartesian matrix, need nnz of each submatrix
+    #use mass matrix to specify sparsity pattern
+    M1_I,M1_J,M1_A = M1_pet.mat().getValuesCSR()
+    M1_NNZ = M1_I[1:]-M1_I[:-1]
+    M2_I,M2_J,M2_A = M2_pet.mat().getValuesCSR()
+    M2_NNZ = M2_I[1:]-M2_I[:-1]
+    M1 = sp.csr_matrix((M1_A,M1_J,M1_I),shape=(M1_sizes[0],M1_global_size[1]))
+    M2 = sp.csr_matrix((M2_A,M2_J,M2_I),shape=M2_sizes)
+    temp2 = sp.kron(M1,M2,format="csr")
+    M_NNZ = temp2.indptr[1:]-temp2.indptr[:-1]
+
+    #now need to mass matrixes for stiffness and RHS
+    ##################################################################
+    ##################################################################
+    #Loading routine for mass and stiffness matrix
+    #set the global mass matrix using CSR
+    M.setValuesCSR(temp2.indptr,temp2.indices,temp2.data)
+    M.assemble()
+    return M_NNZ
+
+def build_stiffness_varying_poisson(V1,V2,kappa,N_dof_2,A):
+    u1 = TrialFunction(V1)
+    v1 = TestFunction(V1)
+    u2 = TrialFunction(V2)
+    v2 = TestFunction(V2)
+    kappa_func = Function(V1)
+    #this stores functions to be intergrated
+    #this is terribly inneficient, maybe look into scipy sparse
+    #or something like this
+
+    #loop through dof_2 and get a N_dof_1xN_dof_1 sparse matrix
+    #each matrix will have same sparsity pattern so get first one then
+    #create numpy to store vals
+
+    #need value at a specific dof_coordinate in second domain
+    kappa_func.vector()[:] = np.array(kappa[0::N_dof_2])
+    #create expressions and assemble linear forms
+    K11 = kappa_func*u1.dx(0)*v1.dx(0)*dx
+    K12 = kappa_func*u1*v1*dx
+    #then save all matrices to list of matrices
+    #since these are sparse maybe take PETSc output and pipe
+    #to scipy sparse matrices
+    #maybe not so easy to program on second loop though
+
+    #K1,K2 are temporary variables to store matrices
+    K1=PETScMatrix()
+    K2 = PETScMatrix()
+    assemble(K11,tensor=K1)
+    assemble(K12,tensor=K2)
+
+    #store sparsity pattern (rows,columns, vals)
+    A1_I,A1_J,temp = K1.mat().getValuesCSR()
+    A2_I,A2_J,temp2 = K2.mat().getValuesCSR()
+    len1 = len(temp)
+    len2 = len(temp2)
+    #create np to store N_dof_2 sets of vals
+    vals1 = np.zeros((len1,N_dof_2))
+    vals2 = np.zeros((len2,N_dof_2))
+    vals1[:,0] = temp
+    vals2[:,0] = temp2
+    #need to loop over nodes in N-dof-2
+    for a in range(1,N_dof_2):
+        #need value at a specific dof_coordinate in second domain
+        kappa_func.vector()[:] = np.array(kappa[a::N_dof_2])
+        #create expressions and assemble linear forms
+        K11 = kappa_func*u1.dx(0)*v1.dx(0)*dx
+        K12 = kappa_func*u1*v1*dx
+        #then save all matrices to list of matrices
+        #since these are sparse maybe take PETSc output and pipe
+        #to scipy sparse matrices
+        #maybe not so easy to program on second loop though
+
+        #need to rebuild each time?
+        K1 = PETScMatrix()
+        K2 = PETScMatrix()
+        assemble(K11,tensor=K1)
+        assemble(K12,tensor=K2)
+
+
+        _,_,temp = K1.mat().getValuesCSR()
+        _,_,temp2 = K2.mat().getValuesCSR()
+
+        vals1[:,a] = temp
+        vals2[:,a] = temp2
+
+
+    #now for each entry in sparse N_dof_1 x N_dof_1 matrix need to evaluate
+    # int_Omega2 fy ... dy
+    #like before, first need to get sparsity patterns
+
+
+    fy = Function(V2)
+
+    fy.vector()[:] = np.array(vals1[0,:])
+
+    K1 = PETScMatrix(MPI.COMM_SELF)
+    K21 = u2*v2*fy*dx
+    assemble(K21,tensor=K1)
+
+
+    K2 = PETScMatrix(MPI.COMM_SELF)
+    fy.vector()[:] = np.array(vals2[0,:])
+    K22 = u2.dx(0)*v2.dx(0)*fy*dx
+    assemble(K22,tensor=K2)
+
+
+    B1_I,B1_J,temp = K1.mat().getValuesCSR()
+    B2_I,B2_J,temp2 = K2.mat().getValuesCSR()
+    #print('B1_I')
+    #print(B1_I)
+    #print(B1_J)
+    #print(temp)
+
+
+    blen1 = len(temp)
+    blen2 = len(temp2)
+
+    dat1 = np.zeros((blen1,len1))
+    dat2 = np.zeros((blen2,len2))
+
+    dat1[:,0] = temp
+    dat2[:,0] = temp2
+
+    #KEY! IDK IF TRUE BUT ASSUMING length of sparse matrixes K1,K2 were same
+    #If not, then will need separate loops
+
+    for i in range(1,len1):
+        fy.vector()[:] = np.array(vals1[i,:])
+
+        K1 = PETScMatrix(MPI.COMM_SELF)
+        K21 = u2*v2*fy*dx
+        assemble(K21,tensor=K1)
+
+
+        K2 = PETScMatrix(MPI.COMM_SELF)
+        fy.vector()[:] = np.array(vals2[i,:])
+        K22 = u2.dx(0)*v2.dx(0)*fy*dx
+        assemble(K22,tensor=K2)
+
+        _,_,temp = K1.mat().getValuesCSR()
+        _,_,temp2 = K2.mat().getValuesCSR()
+
+        dat1[:,i] = temp
+        dat2[:,i] = temp2
+
+
+    Krow,Kcol,Kdat = assemble_global_CSR(A1_I,A1_J,B1_I,B1_J,dat1)
+    Krow2,Kcol2,Kdat2 = assemble_global_CSR(A2_I,A2_J,B2_I,B2_J,dat2)
+    #lastly need to rearrange indeces and rows to give final assignment in A
+
+    #see if sparsity patterns are identical
+    #print(np.sum(Kcol-Kcol2))
+    #print(np.sum(Krow-Krow2))
+
+    Krow=Krow.astype(np.int32)
+    Kcol=Kcol.astype(np.int32)
+
+    #assign values to PETSc matrix
+    A.setValuesCSR(Krow,Kcol,Kdat+Kdat2)
+    A.assemble()
+    return 0
 
--- Action_Balance_HPC/poisson_varying_diffusivity_backup.py
+++ Action_Balance_HPC/poisson_varying_diffusivity_backup.py
@@ -22,7 +22,7 @@ rank = comm.Get_rank()
 nprocs = comm.Get_size()
 # Create cartesian mesh of two intervals and define function spaces
 nx = 400
-ny = 20
+ny = 200
 #set initial time
 t = 0
 #set time step
@@ -57,24 +57,25 @@ M2_pet = PETScMatrix(MPI.COMM_SELF)
 assemble(m2,tensor=M2_pet)
 ###################################################################
 ###################################################################
-#save sizes of subdomain matrices
+#Allocate global mass matrix
+#need to generate global mass matrix to get global matrix layout and sparsity patterns
+#Assemble global matrices (product of each subdomain)
 M1_sizes = M1_pet.mat().getLocalSize()
 M1_global_size =M1_pet.mat().getSize() 
 M2_sizes = M2_pet.mat().getLocalSize()
 
-#calculate sizes of matrix for global domain
-#number of rows/cols on each processor
 local_rows = int(M1_sizes[0]*M2_sizes[0])
 global_rows = int(M1_global_size[0]*M2_sizes[0])
 local_cols = int(M1_sizes[1]*M2_sizes[1])
 global_cols = int(M1_global_size[1]*M2_sizes[1])
-###################################################################
-###################################################################
-#Allocate global mass matrix
-#need to generate global mass matrix to get global matrix layout and sparsity patterns
-#global matrices are product of each subdomain
-M=CF.create_cartesian_mass_matrix(local_rows,global_rows,local_cols,global_cols)
 
+#first I need to create an mpi matrix of the appropriate size and start storing values
+M = PETSc.Mat()
+M.create(comm=comm)
+M.setSizes(([local_rows,global_rows],[local_cols,global_cols]),bsize=1)
+M.setFromOptions()
+M.setType('aij')
+M.setUp()
 #also need global stiffness matrix
 #same exact structure as M
 A = M.duplicate()
@@ -92,8 +93,8 @@ N_dof_2 = len(dof_coordinates2)
 global_dof=CF.cartesian_product_coords(dof_coordinates1,dof_coordinates2)
 x = global_dof[:,0]
 y = global_dof[:,1]
-#get global equation number of any node on entire global boundary
-global_boundary_dofs = CF.fetch_boundary_dofs(V1,V2,dof_coordinates1,dof_coordinates2) + local_range[0]
+#why do we add local range?
+global_boundary_dofs = CF.fetch_boundary_dofs(V1,V2,dof_coordinates1,dof_coordinates2)+ local_range[0]
 ####################################################################
 ####################################################################
 #generate any coefficients that depend on the degrees of freedom
@@ -101,16 +102,188 @@ alpha = 1.0
 kappa = np.exp(alpha*x*y)
 ###################################################################
 ###################################################################
-#Preallocate and load/assemble cartesian mass matrix!
-#now need to mass matrixes for stiffness and RHS, also optionally can out put the nnz
-M_NNZ = CF.build_cartesian_mass_matrix(M1_pet,M2_pet,M1_sizes,M1_global_size,M2_sizes,M)
+#Preallocate!
+#to get nnz of cartesian matrix, need nnz of each submatrix
+#use mass matrix to specify sparsity pattern
+M1_I,M1_J,M1_A = M1_pet.mat().getValuesCSR()
+M1_NNZ = M1_I[1:]-M1_I[:-1]
+M2_I,M2_J,M2_A = M2_pet.mat().getValuesCSR()
+M2_NNZ = M2_I[1:]-M2_I[:-1]
+M1 = sp.csr_matrix((M1_A,M1_J,M1_I),shape=(M1_sizes[0],M1_global_size[1]))
+M2 = sp.csr_matrix((M2_A,M2_J,M2_I),shape=M2_sizes)
+temp2 = sp.kron(M1,M2,format="csr")
+M_NNZ = temp2.indptr[1:]-temp2.indptr[:-1]
+
+#now need to mass matrixes for stiffness and RHS
+M.setPreallocationNNZ(M_NNZ)
 A.setPreallocationNNZ(M_NNZ)
 ##################################################################
 ##################################################################
-#Loading A matrix routine
-CF.build_stiffness_varying_poisson(V1,V2,kappa,N_dof_2,A)
-#set Dirichlet boundary as global boundary
+#Loading routine for mass and stiffness matrix
+#set the global mass matrix using CSR
+M.setValuesCSR(temp2.indptr,temp2.indices,temp2.data)
+M.assemble()
+print('Ownership Range')
+print(M.getOwnershipRange())
+#print(M.getValuesCSR())
+#Loading A matrix
+kappa_func = Function(V1)
+#this stores functions to be intergrated
+#this is terribly inneficient, maybe look into scipy sparse
+#or something like this
+
+#loop through dof_2 and get a N_dof_1xN_dof_1 sparse matrix
+#each matrix will have same sparsity pattern so get first one then
+#create numpy to store vals
+
+#need value at a specific dof_coordinate in second domain
+kappa_func.vector()[:] = np.array(kappa[0::N_dof_2])
+#create expressions and assemble linear forms
+K11 = kappa_func*u1.dx(0)*v1.dx(0)*dx
+K12 = kappa_func*u1*v1*dx
+#then save all matrices to list of matrices
+#since these are sparse maybe take PETSc output and pipe 
+#to scipy sparse matrices
+#maybe not so easy to program on second loop though
+
+#K1,K2 are temporary variables to store matrices
+K1=PETScMatrix()
+K2 = PETScMatrix()
+assemble(K11,tensor=K1)
+assemble(K12,tensor=K2)
+
+#store sparsity pattern (rows,columns, vals)
+A1_I,A1_J,temp = K1.mat().getValuesCSR()
+A2_I,A2_J,temp2 = K2.mat().getValuesCSR()
+len1 = len(temp)
+len2 = len(temp2)
+#create np to store N_dof_2 sets of vals
+vals1 = np.zeros((len1,N_dof_2))
+vals2 = np.zeros((len2,N_dof_2))
+vals1[:,0] = temp
+vals2[:,0] = temp2
+#need to loop over nodes in N-dof-2
+for a in range(1,N_dof_2):
+    #need value at a specific dof_coordinate in second domain
+    kappa_func.vector()[:] = np.array(kappa[a::N_dof_2])
+    #create expressions and assemble linear forms
+    K11 = kappa_func*u1.dx(0)*v1.dx(0)*dx
+    K12 = kappa_func*u1*v1*dx
+    #then save all matrices to list of matrices
+    #since these are sparse maybe take PETSc output and pipe 
+    #to scipy sparse matrices
+    #maybe not so easy to program on second loop though
+    
+    #need to rebuild each time?
+    K1 = PETScMatrix()
+    K2 = PETScMatrix()
+    assemble(K11,tensor=K1)
+    assemble(K12,tensor=K2)
+
+
+    _,_,temp = K1.mat().getValuesCSR()
+    _,_,temp2 = K2.mat().getValuesCSR()
+
+    vals1[:,a] = temp
+    vals2[:,a] = temp2
+
+#print(A1_I)
+#print(A1_J)
+#print(vals1[:,a-1])
+#print(A2_I)
+#print(A2_J)
+#print(vals2[:,a-1])
+
+#now for each entry in sparse N_dof_1 x N_dof_1 matrix need to evaluate
+# int_Omega2 fy ... dy
+#like before, first need to get sparsity patterns
+
+
+fy = Function(V2)
+
+fy.vector()[:] = np.array(vals1[0,:])
+    
+K1 = PETScMatrix(MPI.COMM_SELF)
+K21 = u2*v2*fy*dx
+assemble(K21,tensor=K1)
+    
+        
+K2 = PETScMatrix(MPI.COMM_SELF)
+fy.vector()[:] = np.array(vals2[0,:])
+K22 = u2.dx(0)*v2.dx(0)*fy*dx
+assemble(K22,tensor=K2) 
+
+
+B1_I,B1_J,temp = K1.mat().getValuesCSR()
+B2_I,B2_J,temp2 = K2.mat().getValuesCSR()
+#print('B1_I')
+#print(B1_I)
+#print(B1_J)
+#print(temp)
+
+
+blen1 = len(temp)
+blen2 = len(temp2)
+
+dat1 = np.zeros((blen1,len1))
+dat2 = np.zeros((blen2,len2))
+
+dat1[:,0] = temp
+dat2[:,0] = temp2
+
+#KEY! IDK IF TRUE BUT ASSUMING length of sparse matrixes K1,K2 were same
+#If not, then will need separate loops
+
+for i in range(1,len1):
+    fy.vector()[:] = np.array(vals1[i,:])
+    
+    K1 = PETScMatrix(MPI.COMM_SELF)
+    K21 = u2*v2*fy*dx
+    assemble(K21,tensor=K1)
+    
+        
+    K2 = PETScMatrix(MPI.COMM_SELF)
+    fy.vector()[:] = np.array(vals2[i,:])
+    K22 = u2.dx(0)*v2.dx(0)*fy*dx
+    assemble(K22,tensor=K2) 
+
+    _,_,temp = K1.mat().getValuesCSR()
+    _,_,temp2 = K2.mat().getValuesCSR()
+
+    dat1[:,i] = temp
+    dat2[:,i] = temp2
+
+
+Krow,Kcol,Kdat = CF.assemble_global_CSR(A1_I,A1_J,B1_I,B1_J,dat1)
+Krow2,Kcol2,Kdat2 = CF.assemble_global_CSR(A2_I,A2_J,B2_I,B2_J,dat2)
+#print(Krow)
+#print('K1')
+#print(len(Kcol))
+#print(len(Kdat))
+#print('Num rows')
+#print(len(Krow)-1)
+#print(Kcol)
+#print('K2')
+#print(len(Kcol2))
+#print(len(Kdat2))
+#print('Num rows')
+#print(len(Krow2)-1)
+#print(Kcol2)
+#lastly need to rearrange indeces and rows to give final assignment in A
+
+#see if sparsity patterns are identical
+#print(np.sum(Kcol-Kcol2))
+#print(np.sum(Krow-Krow2))
+
+Krow=Krow.astype(np.int32)
+Kcol=Kcol.astype(np.int32)
+#A.setValuesIJV(I,J,D)
+A.setValuesCSR(Krow,Kcol,Kdat+Kdat2)
+A.assemble()
+#set global boundary
 A.zeroRows(global_boundary_dofs,diag=1)
+#print(A.getValuesCSR())
+
 ##################################################################
 ##################################################################
 #assmble RHS
